1a3e818a6807b048ccb1a368667146d215c39383
doeun <id5eun30@gmail.com>
2025-06-04 22:08:32 +0900

    refactor: CSV 파싱 공통 메서드 추출, 중복 제거

---
+++ src/main/java/cleancode/studycafe/mytobe/io/StudyCafeFileHandler.java
@@ -0,0 +1,56 @@
+package cleancode.studycafe.mytobe.io;
+
+import cleancode.studycafe.mytobe.model.StudyCafeLockerPass;
+import cleancode.studycafe.mytobe.model.StudyCafePass;
+import cleancode.studycafe.mytobe.model.StudyCafePassType;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class StudyCafeFileHandler {
+
+    public List<StudyCafePass> readStudyCafePasses() {
+        return readLines(
+                "src/main/resources/cleancode/studycafe/pass-list.csv",
+                line -> {
+                    String[] values = line.split(",");
+                    return StudyCafePass.of(
+                            StudyCafePassType.valueOf(values[0]),
+                            Integer.parseInt(values[1]),
+                            Integer.parseInt(values[2]),
+                            Double.parseDouble(values[3])
+                    );
+                }
+        );
+    }
+
+    public List<StudyCafeLockerPass> readLockerPasses() {
+        return readLines("src/main/resources/cleancode/studycafe/locker.csv",
+                line -> {
+                    String[] values = line.split(",");
+                    return StudyCafeLockerPass.of(
+                            StudyCafePassType.valueOf(values[0]),
+                            Integer.parseInt(values[1]),
+                            Integer.parseInt(values[2])
+                    );
+                }
+        );
+    }
+
+    private <T> List<T> readLines(String path, Function<String, T> mapper) {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(path));
+            return lines.stream()
+                    .map(mapper)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException("파일을 읽는데 실패했습니다.", e);
+        }
+    }
+}
+
